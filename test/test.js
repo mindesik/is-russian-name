9afc97dfb0a5edff5fac7ccf7b2a53f7f074c8b3
De Sik Min <mindesik@icloud.com>
2020-06-22 15:23:04 +1100

    Add test

---
+++ test/test.js
@@ -0,0 +1,15 @@
+const assert = require('assert')
+const { describe, it } = require('mocha')
+const isRussianName = require('../index')
+
+describe('Main', function() {
+  it('Ivan should return true', function(done) {
+    assert.equal(isRussianName('Ivan'), true)
+    done()
+  })
+  
+  it('John should return false', function(done) {
+    assert.equal(isRussianName('John'), false)
+    done()
+  })
+})
